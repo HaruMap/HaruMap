1fa66dc211fcbc6de101c1494b42c5e8d09d5bdc
yoobineee <yoobinlee27@gmail.com>
2022-11-04 16:40:22 +0900

    change made

--- API_sub_arrival.py
+++ API_sub_arrival.py
@@ -15,7 +15,5 @@ def get_sub_real_time(stationName):
     station = stationName
 
     url = "http://swopenAPI.seoul.go.kr/api/subway/{0}/json/realtimeStationArrival/0/100/{1}".format(API.api.get_sub_real_time_key(),str(station))
-
-    print(url)
     
     return requests.get(url).json()
