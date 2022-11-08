7ec70091d904d2e3b80cc9e618eaa1d1ad749309
yoobineee <yoobinlee27@gmail.com>
2022-11-08 12:06:05 +0900

    subway schedule

---
+++ API_sub_schedule.py
@@ -0,0 +1,92 @@
+import requests
+import json
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+import API.api
+
+# 서울시 역코드로 지하철역별 열차 시간표 정보 검색
+# https://data.seoul.go.kr/dataList/OA-101/A/1/datasetView.do
+# 수인분당선 없음
+# 경의중앙선 있는게있고 없는게 있음
+# 신분당선 없음
+# 외부코드를 역코드로 바꿔야함..! 예) 광화문: 533 -> 2534
+
+import requests
+import pandas as pd
+import math
+from datetime import timedelta
+from datetime import datetime
+from pandasql import sqldf
+
+# station_cd: 역코드
+# today: 평일:1, 토요일:2, 휴일/일요일:3
+# updown: 상행,내선:1, 하행,외선:2
+# current: 시간
+
+# today = datetime.datetime.now()
+# current = today.strftime("%X")
+# today = is_holiday(today)
+# sub_schedule(1007, today, 2, current)
+
+def sub_schedule(station_cd, today, updown, current):
+
+    if len(str(station_cd))==3:
+        station_cd = '0'+str(station_cd)
+    API_KEY = API.api.get_sub_schedule_key()
+    url = f"http://openapi.seoul.go.kr:8088/{API_KEY}/json/SearchSTNTimeTableByIDService/1/2/{station_cd}/{today}/{updown}/"
+    re = requests.get(url)
+    rjson = re.json()
+    total_num = int(rjson['SearchSTNTimeTableByIDService']['list_total_count'])
+    total_num
+
+    station_nm = []
+    arrivetime = []
+    lefttime = []
+    subwaysname = []
+    subwayename = []
+
+    for i in range(1, math.ceil(total_num/1000)+1):
+        end = i*1000
+        start = end-1000+1
+
+        if end > total_num:
+            end = total_num
+        
+        url = f"http://openapi.seoul.go.kr:8088/{API_KEY}/json/SearchSTNTimeTableByIDService/{start}/{end}/{station_cd}/{today}/{updown}/"
+        re = requests.get(url)
+        rjson = re.json()
+
+        for u in rjson['SearchSTNTimeTableByIDService']['row']:
+            station_nm.append(u['STATION_NM'])
+            arrivetime.append(u['ARRIVETIME'])
+            lefttime.append(u['LEFTTIME'])
+            subwaysname.append(u['SUBWAYSNAME'])
+            subwayename.append(u['SUBWAYENAME'])
+
+        df = pd.DataFrame({'도착시간': arrivetime})
+
+        sql = f"select * from df where 도착시간>'{current}'"
+        print(sql)
+
+        df = sqldf(sql)
+
+    for i in range(len(df)):
+        ttttt = df['도착시간'][i]
+        ttttt = ttttt.split(":")
+        Hhour = ttttt[0]
+        Mminute = ttttt[1]
+        Ssecond = ttttt[-1]
+        hour = '00'
+        if Hhour == '24':
+            ttttt = hour + ":" + Mminute + ":" + Ssecond
+        else: ttttt = Hhour + ":" + Mminute + ":" + Ssecond
+        df['도착시간'][i] = ttttt
+
+    for i in range(len(df)):
+        df['도착시간'][i] = datetime.datetime.strptime(df['도착시간'][i], '%H:%M:%S')
+        df['도착시간'][i] = df['도착시간'][i].strftime("%X")
+    
+
+    return df
--- day.py
+++ day.py
@@ -12,5 +12,25 @@ def change_to_day(wait_t):
 
     return Day
 
+# !!pip install pytimekr 필요!!
+# current = datetime.datetime.now()
+# print(current) -> 2022-11-08 11:53:22.783901
+
+from pytimekr import pytimekr
+from datetime import date
+
+def is_holiday(current):
+    holiday = pytimekr.holidays() # holidays 메소드는 리스트 형태로 관련값 반환
+    for i in range(len(holiday)):
+        if holiday[i] == current:
+            datedate = 3
+        else:
+            ddd = str(what_day_is_it(date(current.year, current.month, current.day)))
+            if ddd == 'SAT': datedate = 2
+            elif ddd == 'SUN': datedate = 3
+            else: datedate = 1
+
+    return datedate
+
 
 # 필요.. 없을수도..?
