16fa155d0a8b28c4c68ed4e2e3cd91c5ae3bf74e
epadx <90019742+epadx@users.noreply.github.com>
2022-11-19 13:21:12 +0900

    Add files via upload

---
+++ roadtype/rT_cateval.py
@@ -0,0 +1,41 @@
+def roadType_val(list):
+    list_exc0=[]
+    count=0
+    res=0
+    sum=0
+  
+    for j in range(len(list)):
+        if list[j]!=0:
+            count+=1
+            list_exc0.append(list[j])
+        if count>(len(list)/2):
+            res=9.5
+            return res
+
+
+    for k in range(len(list_exc0)):
+        if list_exc0[k]==23:
+            sum=sum+0.5       #these values can be changed.
+        elif list_exc0[k]==21:
+            sum=sum+1.5
+        elif list_exc0[k]==22:
+            sum=sum+3
+        elif list_exc0[k]==24:
+            sum=sum+9.5
+    
+    res=sum/len(list_exc0)
+
+    return res
+
+
+
+#main
+#list=[0, 23, 23, 0, 0, 22, 22, 23, 23, 0, 22, 22]
+#list=[0,0,0,0,22,22,22,22,22,22,24,24,24,24]
+#list=[21, 22, 23, 0,  0, 22, 23, 22, 24, 24, 0, 0, 0]
+list=[23,23,23,23,23,23,0,23,0]
+
+roadType_val(list)
+
+print(roadType_val(list))
+
